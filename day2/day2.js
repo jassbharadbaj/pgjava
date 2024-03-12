ad91efa4d52b8bb38cfe96677364c6265e603c46
jassbharadbaj <154954007+jassbharadbaj@users.noreply.github.com>
2024-03-12 10:42:30 +0000

    jass

---
+++ day2/day2.js
@@ -0,0 +1,5 @@
+const accountId= 12345;
+let accountName= "Renu Sharma";
+var accountpassword= "jass1234";
+accountcity= "jaipur";
+console.table("accountId","accountName","accountpassword");
---
+++ day4/day4.js
@@ -0,0 +1,6 @@
+let anyNameofChannel = "CareerEngine";
+let anothername = anyNameofChannel;
+// console.log(anothername);
+anothername = "JD WEB SERVICES";
+console.log(anyNameofChannel);
+console.log(anothername);
