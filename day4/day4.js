baa1800a67c9d55a2e4c971ded58807104a4ab89
jassbharadbaj <154954007+jassbharadbaj@users.noreply.github.com>
2024-03-12 11:51:09 +0000

    push

--- day4/day4.js
+++ day4/day4.js
@@ -1,6 +1,14 @@
 let anyNameofChannel = "CareerEngine";
 let anothername = anyNameofChannel;
-// console.log(anothername);
+console.log(anothername);
 anothername = "JD WEB SERVICES";
 console.log(anyNameofChannel);
 console.log(anothername);
+let userone = {
+    email:"jass@gmail.com",
+    upi:"user5@ybl"
+}
+let userTwo = userone;
+userTwo.email ="jass@yahoo.com";
+console.log(userone.email);
+console.log(userTwo.email);
